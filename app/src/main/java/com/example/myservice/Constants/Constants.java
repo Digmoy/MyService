36bd1d862a381eb82303dc7ee53633f0d4c9df8d
Priyanaka-kar-android <digmoy.saha@techprostudio.com>
2021-06-21 22:47:03 +0530

    Media Player

--- app/src/main/java/com/example/myservice/Constants/Constants.java
+++ app/src/main/java/com/example/myservice/Constants/Constants.java
@@ -5,5 +5,4 @@ public class Constants {
     public static final String MUSIC_SERVICE_ACTION_PLAY = "com.example.myservice.play";
     public static final String MUSIC_SERVICE_ACTION_PAUSE = "com.example.myservice.pause";
     public static final String MUSIC_SERVICE_ACTION_STOP = "com.example.myservice.stop";
-
 }
